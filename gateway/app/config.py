efc3e383e5777e3cc41e1efbe8ac9fe667ae3f12
Julian Mosig von Aehrenfeld <julianvonmosig@gmail.com>
2025-06-06 21:36:41 +0200

    remove old api endpoints in gateway

--- gateway/app/config.py
+++ gateway/app/config.py
@@ -6,3 +6,9 @@ class Config:
 
     FRONTEND_HOST = os.getenv("FRONTEND_HOST")
     FRONTEND_PORT = int(os.getenv("FRONTEND_PORT"))
+
+    INDEX_HOST = os.getenv("INDEX_HOST")
+    INDEX_PORT = int(os.getenv("INDEX_PORT"))
+
+    RETRIEVE_HOST = os.getenv("RETRIEVE_HOST")
+    RETRIEVE_PORT = int(os.getenv("RETRIEVE_PORT"))
--- gateway/app/routes/api.py
+++ gateway/app/routes/api.py
@@ -1,30 +1,10 @@
 # app/routes/main.py
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, current_app
 import requests
 from app.utils import hash_repo_url, exists_repo
 
-QDRANT_URL = "http://qdrant:6333" #TODO make it like this using network in compose "http://qdrant:6333"
-RETRIEVE_URL = "http://retrieve:5010"
-INDEX_URL = "http://index:5020"
 api_bp = Blueprint("api", __name__)
-"""
-@api_bp.route("/repo/id", methods=["POST"])
-def id_repo():
-    data = request.get_json()
-    url = data.get("url")
-    url_id = hash_repo_url(url)
-    return {"id": url_id}, 200
 
-# TODO write tests for this method
-# TODO check not only in database, but check if it is currently in a celery worker
-@api_bp.route("/repo/check", methods=["Post"])
-def check_repo():
-    data = request.get_json()
-    url = data.get("url")
-    url_id = hash_repo_url(url)
-    exists = exists_repo(url_id)
-    return {"exists" : exists}, 200
-"""
 @api_bp.route("/repo/query", methods=["POST"])
 def query_repo():
     data = request.get_json()
@@ -32,13 +12,18 @@ def query_repo():
     query = data.get("query")
     url_id = hash_repo_url(url)
 
+    retrieve_host = current_app.config["RETRIEVE_HOST"]
+    retrieve_port = current_app.config["RETRIEVE_PORT"]
+    retrieve_url = "http://" + retrieve_host + ":" + str(retrieve_port)
+
+    index_host = current_app.config["INDEX_HOST"]
+    index_port = current_app.config["INDEX_PORT"]
+    index_url = "http://" + index_host + ":" + str(index_port)
     exists = exists_repo(url_id)
     if exists:
-        #res = requests.post(RETRIEVE_URL + "/health", None)
-        res = requests.post(RETRIEVE_URL + "/repo/retrieve", json = {"id": url_id, "query" : query})
+        res = requests.post(retrieve_url + "/repo/retrieve", json = {"id": url_id, "query" : query})
     else:
-        res = requests.post(INDEX_URL + "/repo/index", json = {"id": url_id, "query" : query}) 
-        # Send request to index microservice, start celery, and return redis process id to frontend for polling
+        res = requests.post(index_url + "/repo/index", json = {"id": url_id, "query" : query}) 
     return res.json()
 
 @api_bp.route("/start-indexing", methods=["POST"])
@@ -47,9 +32,12 @@ def start_indexing():
     url = data.get("url")
     url_id = hash_repo_url(url)
     exists = exists_repo(url_id)
+    index_host = current_app.config["INDEX_HOST"]
+    index_port = current_app.config["INDEX_PORT"]
+    index_url = "http://" + index_host + ":" + str(index_port)
     if not exists:
-        res = requests.post(INDEX_URL + "/index", json={"url" : url, "id" : url_id})
-    return {"status" : "indexing"}, 202 #TODO what does this return
+        res = requests.post(index_url + "/index", json={"url" : url, "id" : url_id})
+    return {"status" : "indexing"}, 202 
 
 @api_bp.route("/status", methods=["POST"])
 def status():
@@ -58,8 +46,11 @@ def status():
     url_id = hash_repo_url(url)
 
     exists = exists_repo(url_id)
+    index_host = current_app.config["INDEX_HOST"]
+    index_port = current_app.config["INDEX_PORT"]
+    index_url = "http://" + index_host + ":" + str(index_port)
     if not exists:
-        res = requests.post(INDEX_URL + "/status", json={"id" : url_id})
+        res = requests.post(index_url + "/status", json={"id" : url_id})
         status = res.json().get("status")
     else:
         status = "done"
