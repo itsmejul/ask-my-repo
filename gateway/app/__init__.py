31434b98c78f484cbffe9d5cb4ed42daf2539ff6
Julian Mosig von Aehrenfeld <julianvonmosig@gmail.com>
2025-06-06 13:40:12 +0200

    update frontend with polling

--- gateway/app/__init__.py
+++ gateway/app/__init__.py
@@ -4,6 +4,10 @@ def create_app(config_class="app.config.Config"):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    from flask_cors import CORS
+     # Allow CORS from your frontend container
+    CORS(app, origins=["http://localhost:8010"])  # TODO add the real domain where it will run here later
+
     from app.routes import api
     app.register_blueprint(api.api_bp)
 
