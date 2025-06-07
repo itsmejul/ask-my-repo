6d91c606f689590ac9861329418b6e9bc902a6f5
Julian Mosig von Aehrenfeld <julianvonmosig@gmail.com>
2025-06-07 16:43:02 +0200

    delete temp dir when cloning fails

--- index/app/tasks.py
+++ index/app/tasks.py
@@ -15,8 +15,8 @@ def index_repo(self, url, url_id):
     redis_host = os.getenv("REDIS_HOST")
     redis_port = os.getenv("REDIS_PORT")
     r = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
+    temp_path = temp_repo_path(url_id)
     try:
-        temp_path = temp_repo_path(url_id) # Path to clone the repo
         clone_repo(repo_url=url, target_dir=temp_path)
 
         documents = read_directory_documents(temp_path)
@@ -27,6 +27,7 @@ def index_repo(self, url, url_id):
         r.set(f"repo_status:{url_id}", "done")
     except Exception as e:
         r.set(f"repo_status:{url_id}", "failed")
+        subprocess.run(["rm", "-rf", temp_path]) #TODO check this works with temp path instead of "./temp" ?
         print("error " + str(e))
     
     return "finished"
